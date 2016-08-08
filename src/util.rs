a542c64cb3dd16b48179d09fba47c4d605e99098
AJ Gardner <mrhota@users.noreply.github.com>
2016-08-07 21:25:29 -0500

    Fix read_cldr_data function
    It would not compile before; I forgot to return an Ok(value).

--- src/util.rs
+++ src/util.rs
@@ -5,17 +5,18 @@ use std::io::Read;
 
 use error::Result;
 
-use serde;
+use serde::de::Deserialize;
 use serde_json;
 use bzip2::read::BzDecoder;
 
-fn read_cldr_data<T: serde::Deserialize>(p: &path::Path, pointer: &str) -> Result<T> {
+fn read_cldr_data<T: Deserialize>(p: &path::Path, pointer: &str) -> Result<T> {
     let f = try!(fs::File::open(p));
     let mut decompressor = BzDecoder::new(f);
     let mut contents = String::new();
     try!(decompressor.read_to_string(&mut contents));
     let json: serde_json::value::Value = try!(serde_json::from_str(&contents));
-    try!(serde_json::from_value(json.pointer(pointer)
-                                    .cloned()
-                                    .unwrap_or(serde_json::Value::Null)))
+    let val = try!(serde_json::from_value(json.pointer(pointer)
+                                              .cloned()
+                                              .unwrap_or(serde_json::Value::Null)));
+    Ok(val)
 }
